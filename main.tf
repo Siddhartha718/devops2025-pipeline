10565b68421539f5c7b84327fe828e661b794310
Siddhartha718 <siddhartha7787@gmail.com>
2025-07-28 16:32:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,94 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket = var.artifact_bucket_name
+  force_destroy = true
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+}
+
+resource "aws_codepipeline" "pipeline" {
+  name     = "DevOpsMasters2025Pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner  = var.github_owner
+        Repo   = var.github_repo
+        Branch = "main"
+        OAuthToken = var.github_token
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      configuration = {
+        ProjectName = aws_codebuild_project.build_project.name
+      }
+    }
+  }
+}
+
+resource "aws_codebuild_project" "build_project" {
+  name          = "DevOpsMastersBuild"
+  description   = "Build project for DevOps Masters"
+  service_role  = aws_iam_role.codepipeline_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:6.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+  }
+
+  source {
+    type = "CODEPIPELINE"
+    buildspec = file("buildspec.yml")
+  }
+}
 
