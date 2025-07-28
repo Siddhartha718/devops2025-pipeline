afcaaa60914eb5763365b99d67540f5e4c8a7319
Siddhartha718 <siddhartha7787@gmail.com>
2025-07-28 16:34:07 +0530

    Update test_pipeline.go

--- test_pipeline.go
+++ test_pipeline.go
@@ -1,1 +1,22 @@
+package test
+
+import (
+    "testing"
+    "github.com/gruntwork-io/terratest/modules/terraform"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestTerraformPipeline(t *testing.T) {
+    t.Parallel()
+
+    terraformOptions := &terraform.Options{
+        TerraformDir: "../",
+    }
+
+    defer terraform.Destroy(t, terraformOptions)
+    terraform.InitAndApply(t, terraformOptions)
+
+    pipelineName := terraform.Output(t, terraformOptions, "pipeline_name")
+    assert.Contains(t, pipelineName, "DevOpsMasters2025Pipeline")
+}
 
