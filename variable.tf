ef6df10d1f2d723e037fe4336951e45af2624db8
Siddhartha718 <siddhartha7787@gmail.com>
2025-07-28 16:33:08 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,21 @@
+variable "artifact_bucket_name" {
+  description = "S3 bucket name for storing CodePipeline artifacts"
+  type        = string
+}
+
+variable "github_owner" {
+  description = "GitHub username or organization"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+}
+
+variable "github_token" {
+  description = "GitHub personal access token"
+  type        = string
+  sensitive   = true
+}
 
